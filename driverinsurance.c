0c1bfa8fa03dd85e1335e2f6f891cf12a4f49280
mk5686 <59549846+mk5686@users.noreply.github.com>
2020-12-03 12:57:43 +0530

    Update driverinsurance.c

--- driverinsurance.c
+++ driverinsurance.c
@@ -7,7 +7,7 @@ int main()
 	printf("Enter age ,gender,marital status");
 	scanf("%d%c%c",&age,&gender,&ms);
 
-	if((ms =='M')||(ms == 'U'&& gender=='M' && age>30)||(ms =="U"||gender == "F" && age >25))
+	if((ms =='M')||(ms == 'U'&& gender=='M' && age>30)||(ms =='U'||gender == 'F' && age >25))
 		printf("Driver should be insured");
 	else
 		printf("Driver should not be insured");
